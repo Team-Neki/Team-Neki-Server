f6b4721a836b1d47387cc5023da2c81ee1733a5d
darren <darren@darrenui-MacBookPro.local>
2025-12-18 14:20:22 +0900

    fix: @Repository 제거

--- src/main/kotlin/com/yapp2app/auth/application/repository/UserRepository.kt
+++ src/main/kotlin/com/yapp2app/auth/application/repository/UserRepository.kt
@@ -3,9 +3,7 @@ package com.yapp2app.auth.application.repository
 import com.yapp2app.auth.domain.entity.User
 import com.yapp2app.auth.domain.enums.ProviderType
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface UserRepository : JpaRepository<User, Long> {
     fun findByEmailAndProviderType(email: String, providerType: ProviderType): User?
 }
