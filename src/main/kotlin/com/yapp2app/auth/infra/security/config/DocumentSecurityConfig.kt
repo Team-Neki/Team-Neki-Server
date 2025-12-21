148b56e280d08fddbab7125e893b81453f618b3b
Koo <koosco136@gmail.com>
2025-12-22 02:08:43 +0900

    test: local test용 object CRD API 추가

--- src/main/kotlin/com/yapp2app/auth/infra/security/config/DocumentSecurityConfig.kt
+++ src/main/kotlin/com/yapp2app/auth/infra/security/config/DocumentSecurityConfig.kt
@@ -8,6 +8,7 @@ import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfigurationSource
 
 /**
  * Swagger 및 API 문서 관련 보안 설정
@@ -17,14 +18,28 @@ import org.springframework.security.web.SecurityFilterChain
 class DocumentSecurityConfig(
     private val authTokenProvider: AuthTokenProvider,
     private val appProperties: AppProperties,
+    private val corsConfigurationSource: CorsConfigurationSource,
 ) {
+
+    /**
+     * token 발급 전 test config
+     * token 발급 로직 후 삭제
+     */
+    @Deprecated("test config")
+    @Bean
+    @Order(-1) // 가장 먼저 적용되도록 우선순위 설정
+    fun testSecurityFilterChain(http: HttpSecurity): SecurityFilterChain = http.securityMatcher("/api/media/test/**")
+        .csrf { it.disable() }
+        .cors { it.configurationSource(corsConfigurationSource) }
+        .authorizeHttpRequests { it.anyRequest().permitAll() }
+        .build()
 
     @Bean
     @Order(0)
     fun documentSecurityFilterChain(http: HttpSecurity): SecurityFilterChain =
         http.securityMatcher("/swagger-ui/**", "/v3/api-docs/**")
             .csrf { it.disable() }
-            .cors { it.disable() } // TODO : cors 설정 추가
+            .cors { it.configurationSource(corsConfigurationSource) }
             .logout { it.disable() }
             .authorizeHttpRequests { it.anyRequest().permitAll() }
             .build()
@@ -34,7 +49,7 @@ class DocumentSecurityConfig(
     fun apiSecurityFilterChain(http: HttpSecurity): SecurityFilterChain = http
         .securityMatcher("/api/**")
         .csrf { it.disable() }
-        .cors { /* CORS 설정 */ }
+        .cors { it.configurationSource(corsConfigurationSource) }
         .authorizeHttpRequests {
             it.requestMatchers("/api/auth/**").permitAll()
             it.anyRequest().authenticated()
