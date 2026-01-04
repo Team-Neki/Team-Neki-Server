50dfc34f0edc729d28cdda17c304a84227de7719
Darren4641 <zxz4641@gmail.com>
2026-01-04 19:37:31 +0900

    fix: JasyptTest 테스트코드 yml 의존성 제거

--- src/test/kotlin/com/yapp2app/JasyptTest.kt
+++ src/test/kotlin/com/yapp2app/JasyptTest.kt
@@ -1,14 +1,27 @@
 package com.yapp2app
 
-import org.jasypt.encryption.StringEncryptor
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
 @ActiveProfiles("test")
-class JasyptTest(@Autowired private val jasyptStringEncryptor: StringEncryptor) {
+class JasyptTest {
+
+    private val jasyptStringEncryptor = PooledPBEStringEncryptor().apply {
+        setConfig(
+            SimpleStringPBEConfig().apply {
+                password = "testPasswordForJasypt"
+                algorithm = "PBEWithHmacSHA512AndAES_256"
+                setKeyObtentionIterations("1000")
+                setPoolSize("1")
+                providerName = "SunJCE"
+                setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator")
+                setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator")
+                stringOutputType = "base64"
+            },
+        )
+    }
 
     @Test
     fun jasyptGeneratTest() {
@@ -16,7 +29,7 @@ class JasyptTest(@Autowired private val jasyptStringEncryptor: StringEncryptor) 
         println()
 
         // 기타 암호화할 값
-        val text = "2uYKrIx7hFeZDoEyiXvWSY29TvSGfevQ"
+        val text = "test_text"
         val encryptedText = jasyptStringEncryptor.encrypt(text)
         println("   원본: $text")
         println("   암호화: ENC($encryptedText)")
