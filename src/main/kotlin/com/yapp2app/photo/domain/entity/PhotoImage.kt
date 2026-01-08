fba1645a6f4af6fecc645ff8137a88df6c665737
Koo <koosco136@gmail.com>
2026-01-08 18:07:26 +0900

    feat: photo domain entity 추가

---
+++ src/main/kotlin/com/yapp2app/photo/domain/entity/PhotoImage.kt
@@ -0,0 +1,32 @@
+package com.yapp2app.photo.domain.entity
+
+import com.yapp2app.common.domain.BaseTimeEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+/**
+ * fileName       : PhotoImage
+ * author         : koo
+ * date           : 2025. 12. 23. 오후 7:13
+ * description    : 사용자의 사진 엔티티. url 대신 mediaId로 접근
+ */
+@Entity
+@Table(name = "TB_photo_image")
+class PhotoImage(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "user_id", nullable = false)
+    val userId: Long,
+
+    @Column(name = "media_id", nullable = false)
+    val mediaId: Long,
+
+    @Column(name = "folder_id")
+    var folderId: Long? = null,
+) : BaseTimeEntity()
