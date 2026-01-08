625c5fbef763c354efbb7cc557362334b42641a9
Koo <koosco136@gmail.com>
2026-01-08 18:12:59 +0900

    feat: Local Media Client 구현체 추가

---
+++ src/main/kotlin/com/yapp2app/photo/infra/client/LocalMediaClient.kt
@@ -0,0 +1,61 @@
+package com.yapp2app.photo.infra.client
+
+import com.yapp2app.media.application.command.ConfirmMediaUploadedCommand
+import com.yapp2app.media.application.command.DeleteMediaCommand
+import com.yapp2app.media.application.command.DeleteMediasCommand
+import com.yapp2app.media.application.command.GetMediasCommand
+import com.yapp2app.media.application.usecase.ConfirmMediaUploadedUseCase
+import com.yapp2app.media.application.usecase.DeleteMediaUseCase
+import com.yapp2app.media.application.usecase.GetMediasUseCase
+import com.yapp2app.photo.application.port.MediaClientPort
+import com.yapp2app.photo.application.port.MediaClientPort.*
+import org.springframework.stereotype.Component
+
+/**
+ * fileName       : LocalMediaClient
+ * author         : koo
+ * date           : 2026. 1. 3. 오전 12:00
+ * description    : monolithic architecture media client
+ * - media service 분리 시 OpenFeign, EventPublisher/Consumer로 변경
+ */
+@Component
+class LocalMediaClient(
+    private val confirmMediaUploadedUseCase: ConfirmMediaUploadedUseCase,
+    private val getMediasUseCase: GetMediasUseCase,
+    private val deleteMediaUseCase: DeleteMediaUseCase,
+) : MediaClientPort {
+
+    override fun verifyMediaUploaded(ownerId: Long, mediaId: Long): MediaAvailability {
+        val result = confirmMediaUploadedUseCase.execute(
+            ConfirmMediaUploadedCommand(
+                ownerId = ownerId,
+                mediaId = mediaId,
+            ),
+        )
+
+        return if (result.success) {
+            MediaAvailability.AVAILABLE
+        } else {
+            MediaAvailability.UNAVAILABLE
+        }
+    }
+
+    override fun getMediaBinaries(ownerId: Long, mediaIds: List<Long>): List<MediaInfo> {
+        val result = getMediasUseCase.execute(GetMediasCommand(ownerId, mediaIds))
+
+        return result.medias.map {
+            MediaInfo(
+                mediaId = it.mediaId,
+                binaryData = it.binaryData,
+            )
+        }.toList()
+    }
+
+    override fun deleteMedia(ownerId: Long, mediaId: Long) {
+        deleteMediaUseCase.execute(DeleteMediaCommand(ownerId, mediaId))
+    }
+
+    override fun deleteMedias(ownerId: Long, mediaIds: List<Long>) {
+        deleteMediaUseCase.execute(DeleteMediasCommand(ownerId, mediaIds))
+    }
+}
