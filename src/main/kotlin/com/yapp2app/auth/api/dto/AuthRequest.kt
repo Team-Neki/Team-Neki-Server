02d31cc745efcfd209f2d2b34671412c2dc0ee12
darren <darren@darrenui-MacBookPro.local>
2026-01-09 17:20:55 +0900

    fix: Valid 어노테이션 @field로 변경

--- src/main/kotlin/com/yapp2app/auth/api/dto/AuthRequest.kt
+++ src/main/kotlin/com/yapp2app/auth/api/dto/AuthRequest.kt
@@ -2,6 +2,7 @@ package com.yapp2app.auth.api.dto
 
 import com.yapp2app.user.domain.enums.ProviderType
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 /**
  * fileName       : AuthRequest
@@ -9,8 +10,8 @@ import jakarta.validation.constraints.NotBlank
  * date           : 2025. 12. 26. 18:05
  * description    : 인증/인가 관련 요청 body
  */
-data class CreateAuthRequest(@NotBlank(message = "ID 토큰은 필수입니다") val idToken: String)
+data class CreateAuthRequest(@field:NotBlank(message = "ID 토큰은 필수 입니다") val idToken: String)
 
 data class LoginRequest(val oid: Long, val providerType: ProviderType)
 
-data class RefreshTokenRequest(@NotBlank(message = "Refresh 토큰은 필수입니다") val refreshToken: String)
+data class RefreshTokenRequest(@field:NotBlank(message = "Refresh 토큰은 필수입니다") val refreshToken: String)
