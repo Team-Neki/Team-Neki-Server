6ac980fa0b902db9b735f005794b79208374044b
Darren4641 <zxz4641@gmail.com>
2026-01-24 20:05:11 +0900

    fix: getPhotoBoothsByPoint API Default 값 Converter에서 수행

--- src/main/kotlin/com/yapp2app/map/api/dto/MapRequest.kt
+++ src/main/kotlin/com/yapp2app/map/api/dto/MapRequest.kt
@@ -68,7 +68,4 @@ data class GetPointLocationRequest(
 
     @field:Schema(description = "브랜드 ID 리스트 (nullable [] 이면 모든 브랜드) ", example = "[1, 2, 3]")
     val brandIds: List<Long>? = null,
-) {
-    fun getLongitudeOrDefault(): Double = longitude ?: 127.0276
-    fun getLatitudeOrDefault(): Double = latitude ?: 37.4979
-}
+)
