bb926c1cf69f2ae3971551bdae262953adcc41e6
Darren4641 <zxz4641@gmail.com>
2026-01-26 23:17:28 +0900

    fix: Spotless 적용

--- src/main/kotlin/com/yapp2app/photo/api/dto/GetPhotosResponse.kt
+++ src/main/kotlin/com/yapp2app/photo/api/dto/GetPhotosResponse.kt
@@ -34,5 +34,5 @@ data class GetFavoriteSummaryResponse(
     @field:Schema(description = "가장 최근 즐겨찾기 사진 URL", example = "https://dev-yapp.suitestudy.com:4641/file/image/...")
     val latestImageUrl: String?,
     @field:Schema(description = "즐겨찾기 갯수", example = "1")
-    val totalCount: Long
+    val totalCount: Long,
 )
