12b9e507b6305e9b6d2cbf4fb73d1979421fabda
Koo <koosco136@gmail.com>
2026-01-31 00:51:04 +0900

    chore: user domain request 파일 병합

--- src/main/kotlin/com/yapp2app/user/api/dto/UserRequest.kt
+++ src/main/kotlin/com/yapp2app/user/api/dto/UserRequest.kt
@@ -2,20 +2,24 @@ package com.yapp2app.user.api.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 /**
- * fileName       : UpdateUserRequest
+ * fileName       : UserRequest
  * author         : koo
- * date           : 2026. 1. 28. 오후 1:06
+ * date           : 2026. 1. 30. 오후 11:58
  * description    :
  */
 data class UpdateUserRequest(
-    @field:Schema(description = "프로필 이미지로 설정할 mediaId, null일 경우 기본 이미지로 변경")
-    val mediaId: Long? = null,
-
     @field:Schema(description = "공백을 포함해 10글자 이하로 변경할 닉네임을 설정합니다", example = "새로운닉네임")
     @field:Size(max = 10, message = "닉네임은 공백 포함 10자 이하여야 합니다.")
     @field:NotBlank(message = "닉네임은 공백 포함 10자 이하여야 합니다")
     val name: String,
+)
+
+data class UpdateUserProfileImageRequest(
+    @field:Schema(description = "프로필 이미지로 설정할 mediaId, null일 경우 기본 이미지로 변경")
+    @field:NotNull(message = "mediaId는 null이 될 수 없습니다.")
+    val mediaId: Long? = null,
 )
