000427d8eaf94ccdb000f4c20b9093ee3443bd35
Darren4641 <zxz4641@gmail.com>
2026-01-31 15:21:07 +0900

    fix: 소셜로그인 providerType 타입 String으로 변경 (대소문자 모두 허용을 위해)

--- src/main/kotlin/com/yapp2app/auth/api/converter/AuthCommandConverter.kt
+++ src/main/kotlin/com/yapp2app/auth/api/converter/AuthCommandConverter.kt
@@ -4,18 +4,25 @@ import com.yapp2app.auth.api.dto.CreateAuthRequest
 import com.yapp2app.auth.api.dto.RefreshTokenRequest
 import com.yapp2app.auth.application.command.RefreshTokenCommand
 import com.yapp2app.auth.application.command.RegisterOauthUserCommand
+import com.yapp2app.common.api.dto.ResultCode
+import com.yapp2app.common.exception.BusinessException
 import com.yapp2app.user.domain.enums.ProviderType
 import org.springframework.stereotype.Component
 
 @Component
 class AuthCommandConverter {
 
-    fun toCreateAuthCommand(request: CreateAuthRequest, providerType: ProviderType): RegisterOauthUserCommand =
+    fun toCreateAuthCommand(request: CreateAuthRequest, providerType: String): RegisterOauthUserCommand =
         RegisterOauthUserCommand(
             idToken = request.idToken!!,
-            providerType = providerType,
+            providerType = toProviderType(providerType),
         )
 
     fun toRefreshTokenCommand(request: RefreshTokenRequest): RefreshTokenCommand =
         RefreshTokenCommand(request.refreshToken!!)
+
+    private fun toProviderType(providerType: String): ProviderType = runCatching {
+        ProviderType.valueOf(providerType.uppercase())
+    }
+        .getOrElse { throw BusinessException(ResultCode.INVALID_PARAMETER) }
 }
