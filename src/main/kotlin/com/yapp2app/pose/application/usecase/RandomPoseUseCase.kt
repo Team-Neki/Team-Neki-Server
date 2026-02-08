82faa293f47221352fba24df44d726af0c92e137
Darren4641 <zxz4641@gmail.com>
2026-02-08 20:27:10 +0900

    fix: 랜덤포즈 조회할 포즈 없을 시 문구 수정

--- src/main/kotlin/com/yapp2app/pose/application/usecase/RandomPoseUseCase.kt
+++ src/main/kotlin/com/yapp2app/pose/application/usecase/RandomPoseUseCase.kt
@@ -21,13 +21,13 @@ class RandomPoseUseCase(
     fun execute(command: GetRandomPoseCommand): GetPoseResult {
         val count = poseRepository.countPoses(command.headCount, command.excludeIds)
         if (count == 0L) {
-            throw BusinessException(ResultCode.NOT_FOUND)
+            throw BusinessException(ResultCode.NO_MORE_RANDOM_POSE)
         }
 
         val randomOffset = randomGenerator.nextLong(count)
 
         val pose = poseRepository.findPoseByOffset(randomOffset, command.headCount, command.excludeIds)
-            ?: throw BusinessException(ResultCode.NOT_FOUND)
+            ?: throw BusinessException(ResultCode.NO_MORE_RANDOM_POSE)
 
         val isScraped = scrapPoseRepository.existsOwnedPoseScrap(command.userId, pose.id!!)
 
