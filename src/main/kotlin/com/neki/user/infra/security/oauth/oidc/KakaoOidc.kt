08fc98e54e0c87ad9d4832ec73b7ffec7869552e
Darren4641 <zxz4641@gmail.com>
2026-03-23 22:56:48 +0900

    test: gitOps용 빌드 실패 테스트

--- src/main/kotlin/com/neki/user/infra/security/oauth/oidc/KakaoOidc.kt
+++ src/main/kotlin/com/neki/user/infra/security/oauth/oidc/KakaoOidc.kt
@@ -40,7 +40,6 @@ class KakaoOidc(
         val cached = authRedisCacheAdapter.getPublicKeys(AuthCacheKeys.KAKAO_OIDC_KEY)
 
         if (cached != null) {
-            log.info("Found cached OIDC key") // TODO 임시 코드
             return cached
         }
 
